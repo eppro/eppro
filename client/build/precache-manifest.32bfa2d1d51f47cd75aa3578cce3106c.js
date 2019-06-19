343e2cd3972f97fe9544d434b52f6a7c26d40393
PRJ88 <pranjal.ostwal.po@gmail.com>
2019-06-19 18:42:36 +0530

    added file

---
+++ client/build/precache-manifest.32bfa2d1d51f47cd75aa3578cce3106c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87679bbb6af2c7c99087b87168571d7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77a2d00feb011422a27d",
+    "url": "/static/js/2.9f4943b1.chunk.js"
+  },
+  {
+    "revision": "9b1e9c14ca083da68a31",
+    "url": "/static/js/main.6a7d4ab4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
